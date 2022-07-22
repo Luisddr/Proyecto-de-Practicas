0ce2effacf788ac2bcdc5161a5da48d675c09b29
Luis Carlos <luisdediosr@gmail.com>
2022-07-22 18:01:14 -0500

    changes styling in cards and improves dark mode

--- src/global.styles.js
+++ src/global.styles.js
@@ -12,16 +12,7 @@ body{
     }
 }
 
-.dark-content{
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    padding: 20px 40px;
-    background-color: black;
 
-    @media screen and (max-width: 800px){
-        padding: 10px;
-    }
-
-}
 
 a{
     text-decoration: none;
